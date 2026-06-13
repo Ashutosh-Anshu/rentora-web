abb0a48f58c9b794e4d2f0033c2c3269269d9412
Ashutosh Anshu <ashutoshanshu412541@gmail.com>
2026-06-13 14:36:52 +0530

    Add basic component and folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home),
+        children: [
+            {
+                path: '',
+                title: 'Home',
+                loadComponent: () => import('./layout/main-layout/main-layout').then(m => m.MainLayout)
+            }
+
+        ]
+    }
+];
---
+++ src/app/layout/index.ts
@@ -0,0 +1,4 @@
+export * from './header/header';
+export * from './sidebar/sidebar';
+export * from './footer/footer';
+export * from './main-layout/main-layout';
